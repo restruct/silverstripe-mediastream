a155b0e81f5374e13f47fdc5594dc000f64f88f8
Itayi Patrick Chito-voro <patrick@chitosystems.com>
2021-12-03 18:38:58 +0200

    change Tasks namespace

--- src/Feed/Tasks/SyncJob.php
+++ src/Feed/Tasks/SyncJob.php
@@ -1,45 +1,44 @@
 <?php
 
-namespace Restruct\Silverstripe\MediaStream {
+namespace Restruct\Silverstripe\MediaStream\Tasks;
 
-    use SilverStripe\Dev\BuildTask;
-    use SilverStripe\Dev\Debug;
+use SilverStripe\Dev\BuildTask;
+use SilverStripe\Dev\Debug;
 
-    class SyncJob extends BuildTask
+class SyncJob extends BuildTask
+{
+
+    /**
+     * Make the completed job queue another of itself after x seconds
+     */
+    private static $regenerate_time = 3600; // default every hour
+
+    private $jobStartedTimeStamp;
+
+    /**
+     * @var array
+     */
+    private $mediaInputs;
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return 'Sync (Social) MediaStream';
+    }
+
+    public function run($request)
     {
 
-        /**
-         * Make the completed job queue another of itself after x seconds
-         */
-        private static $regenerate_time = 3600; // default every hour
+        $oMediaStream = MediaStream::get()->filter('Enabled', true);
 
-        private $jobStartedTimeStamp;
+        /** @var MediaStream $item */
+        foreach ( $oMediaStream as $item ) {
 
-        /**
-         * @var array
-         */
-        private $mediaInputs;
-
-        /**
-         * @return string
-         */
-        public function getTitle()
-        {
-            return 'Sync (Social) MediaStream';
+            echo( "SYNCING: {$item->Name} <br>" );
+            echo( $item->fetchUpdates() ?: 'OK' );
         }
 
-        public function run($request)
-        {
-
-            $oMediaStream = MediaStream::get()->filter('Enabled', true);
-
-            /** @var MediaStream $item */
-            foreach ( $oMediaStream as $item ) {
-
-                echo( "SYNCING: {$item->Name} <br>" );
-                echo( $item->fetchUpdates() ?: 'OK' );
-            }
-
-        }
     }
 }
